4077a36934bdbf648a1981ee72dba2997f67a43e
mathyan <mathyan.cvitanovic@outlook.com>
2024-04-15 00:02:33 +0200

    Append last

--- init.lua
+++ init.lua
@@ -737,6 +737,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'copilot' },
         },
       }
     end,

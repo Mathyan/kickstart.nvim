6f7966b6346131488e2150816e21fccbda888e6b
mathyan <mathyan.cvitanovic@outlook.com>
2024-04-14 20:29:33 +0200

    Added harpoon typescript tools and lazygit

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -376,6 +376,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sG', builtin.git_files, { desc = '[S]earch [G]it files' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -630,7 +631,8 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
       },
     },
   },
@@ -655,12 +657,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
@@ -838,16 +840,16 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,88 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    {
+      '<leader>a',
+      function()
+        require('harpoon'):list():add()
+      end,
+      mode = 'n',
+      desc = '[A]dd harpoon mark',
+    },
+    {
+      '<leader>h',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(require('harpoon'):list())
+      end,
+      mode = 'n',
+      desc = 'Toggle [H]arpoon quick menu',
+    },
+
+    -- Select first mark
+    {
+      '<C-j>',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+      mode = 'n',
+      desc = 'Select first Harpoon mark',
+    },
+
+    -- Select second mark
+    {
+      '<C-k>',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+      mode = 'n',
+      desc = 'Select second Harpoon mark',
+    },
+
+    -- Select third mark
+    {
+      '<C-l>',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+      mode = 'n',
+      desc = 'Select third Harpoon mark',
+    },
+
+    -- Select fourth mark
+    {
+      '<C-;>',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+      mode = 'n',
+      desc = 'Select fourth Harpoon mark',
+    },
+
+    -- Navigate to previous mark
+    {
+      '<C-S-P>',
+      function()
+        require('harpoon'):list():prev()
+      end,
+      mode = 'n',
+      desc = 'Navigate to previous Harpoon mark',
+    },
+
+    -- Navigate to next mark
+    {
+      '<C-S-N>',
+      function()
+        require('harpoon'):list():next()
+      end,
+      mode = 'n',
+      desc = 'Navigate to next Harpoon mark',
+    },
+  },
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup()
+  end,
+}
